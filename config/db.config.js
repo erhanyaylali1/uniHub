f903a553d6bd197b445d80fd675accd67848f306
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2021-10-17 11:37:50 +0300

    .
    .

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
-    HOST: "unihun.czbknzpnkjsg.us-east-2.rds.amazonaws.com",
-    USER: "admin",
-    PASSWORD: "efes1998",
-    DB:  "unihub",
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB,
     dialect: "mysql",
     pool: {
       max: 5,
